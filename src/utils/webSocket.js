186441172650bb4412c8a00c2916d0b37c9b29f0
Gh1ta <salame.ghita@inemail.ine.inpt.ma>
2022-11-12 12:48:07 +0100

    working app

--- src/utils/webSocket.js
+++
@@ -1,5 +0,0 @@
-import socketIOClient from 'socket.io-client';
-
-let socket = socketIOClient('/', {path: '/socket/', transports: ['websocket', 'polling', 'flashsocket']});
-
-export default socket;
